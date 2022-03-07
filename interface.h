80b79b166c3c266599deeb32518bb294439a618c
Thomas Roiseux <thomas.roiseux@ensiie.fr>
2022-03-07 19:58:00 +0100

    [Tâche A.3] Ajout de deux prototypes

--- interface.h
+++ interface.h
@@ -1,3 +1,11 @@
+/**
+ * @file interface.h
+ * @author Thomas Roiseux (thomas.roiseux@ensiie.fr)
+ * @brief Interface entre le plateau de jeu et le joueur
+ * @version 0.1
+ * @date 2022-03-07
+ */
+
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
@@ -20,5 +28,22 @@ void affiche_plateau(Plateau p);
  * @author Thomas Roiseux
  */
 void affiche_main(Faction f);
+
+/**
+ * @brief Fonction qui demande si la faction pointée par f veut utiliser son option
+ * @author Thomas Roiseux
+ * @param f faction en jeu
+ * @return 0 non
+ * @return 1 oui
+ */
+int utiliser_option(Faction f);
+
+/**
+ * @brief Fonction qui demande la carte qui sera posée face cachée sur le plateau.
+ * @author Thomas Roiseux
+ * @param f faction en jeu
+ * @return Carte carte choisie par la faction f et posée sur le plateau.
+ */
+Carte a_poser_face_cachee_sur_plateau(Faction f);
 
 #endif //INTERFACE_H
