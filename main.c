672a66a5c7c6c99ddbec9abdbf73e5d615e610b7
Thomas Roiseux <thomas.roiseux@ensiie.fr>
2022-03-07 20:00:11 +0100

    [Tâche A.1] Ajout de commentaires

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/**
+ * @file main.c
+ * @author Thomas Roiseux (thomas.roiseux@ensiie.fr)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-07
+ */
+
 #include <stdlib.h>
 
 #include "interface.h"
@@ -5,6 +13,14 @@
 #include "faction.h"
 #include "plateau.h"
 
+
+/**
+ * @brief Fonction principale qui controle le programme.
+ * 
+ * @param argc Nombre d'arguments passés à l'exécutable.
+ * @param argv Liste de ces arguments
+ * @return int Code de retour
+ */
 int main(int argc, char *argv[])
 {
     
