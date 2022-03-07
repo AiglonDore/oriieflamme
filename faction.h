787632f3118494f913ca41905ad92c12f62e4776
Nour Elbessi <nelbessi98@gmail.com>
2022-03-07 21:12:51 +0100

    [Tache A.2] Definition de faction

---
+++ faction.h
@@ -0,0 +1,15 @@
+#ifndef FACTION_H
+#define FACTION_H
+
+/**
+ * @brief Déclaration abstraite des factions
+ * @author Nour ELBESSI
+ * @date 07/03/2022
+ */
+
+// struct for a card
+typedef struct StructureFactions *Faction;
+
+
+
+#endif
