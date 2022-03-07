d08d42ef1f098b80a76688d8846e06cc8d7cb87c
Nour Elbessi <nelbessi98@gmail.com>
2022-03-07 20:57:30 +0100

    CrÃation de carte.h avec dÃnition de son type

---
+++ carte.h
@@ -0,0 +1,15 @@
+#ifndef CARTE_H
+#define CARTE_H
+
+/**
+ * @brief Déclaration des cartes en utilisant un struct
+ * @author Nour ELBESSI
+ * @date 07/03/2022
+ */
+
+// struct for a card
+typedef struct StructureCarte *Carte;
+
+
+
+#endif
