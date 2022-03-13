d0b3e8560d39b5526c1ffe624bbb8a91ba47a78f
Valentin Gardel <valentin.gardel@ensiie.fr>
2022-03-13 15:46:56 +0100

    [Tâche A.3] Dépôt de l'interface pour le plateau de jeu

---
+++ plateau.h
@@ -0,0 +1,88 @@
+/**
+ * @file plateau.h
+ * @author Valentin Gardel (valentin.gardel@ensiie.fr)
+ * @brief Interface pour gérer le plateau de jeu et le déroulement de la partie
+ * @version 0.1
+ * @date 2022-03-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _PLATEAU_H
+#define _PLATEAU_H
+#include "carte.h"
+#include "faction.h"
+
+/**
+ * @brief type concret pour les listes de factions
+ * 
+ */
+
+struct bucket {
+  Faction f;
+  struct bucket *next;
+};
+
+typedef struct bucket * list;
+
+/**
+ * @brief type abstrait pour le plateau de jeu
+ * 
+ */
+
+typedef struct s_plateau * Plateau;
+
+/**
+ * @brief crée un plateau et les deux factions qui joueront dessuss
+ * @return le plateau initialisé
+ * 
+ */
+
+Plateau init_plateau();
+
+/**
+ * @brief libère la mémoire associée au plateau de jeu et aux deux factions
+ * @param p plateau valide (le pointeur doit être valide)
+ * 
+ */
+
+void libere_plateau(Plateau p);
+
+/**
+ * @brief initialise une nouvelle manche ou indique que le jeu est terminé
+ * @param p plateau valide (le pointeur doit être valide)
+ * @return 0 si la partie continue, 1 si le jeu est terminé
+ * 
+ */
+
+int nouvelle_manche(Plateau p);
+
+/**
+ * @brief renvoie la liste des deux factions du jeu
+ * @param p plateau valide (le pointeur doit être valide)
+ * @return la liste des des factions du jeu
+ * 
+ */
+
+list factions_plateau(Plateau p);
+
+/**
+ * @brief permet à une faction de poser une carte face cachée sur le plateau
+ * @param c carte valide
+ * @param p plateau valide (le pointeur doit être valide)
+ * 
+ */
+
+void poser_carte(Carte c, Plateau p);
+
+/**
+ * @brief permet à une faction de retourner une carte face visible et activer son effet
+ * @param c carte valide
+ * @param p plateau valide (le pointeur doit être valide)
+ * 
+ */
+
+void retourner_carte(Carte c, Plateau p);
+
+#endif // PLATEAU_H
