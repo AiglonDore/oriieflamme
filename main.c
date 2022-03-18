3598166ca668563fbf97b017fb3b6e69865b1bb6
Thomas Roiseux <thomas.roiseux@ensiie.fr>
2022-03-18 16:47:59 +0100

    [Tâche A.1] Implémentation du retournement de carte

--- main.c
+++ main.c
@@ -37,8 +37,17 @@ void phase1(Plateau p)
     {
         melanger(factions.right);
     }
-
-
+    for (int i = 8; i > 0; i--)
+    {
+        affiche_main(factions.left);
+        Carte a_poser = a_poser_face_cachee_sur_plateau(factions.left);
+        IntCouple position = demande_position(p);
+        poser_carte(a_poser,p,position.left,position.right);
+        affiche_main(factions.right);
+        a_poser = a_poser_face_cachee_sur_plateau(factions.right);
+        position = demande_position(p);
+        poser_carte(a_poser,p,position.left,position.right);
+    }
 }
 
 /**
@@ -48,23 +57,27 @@ void phase1(Plateau p)
  */
 void phase2(Plateau p)
 {
-
+    Carte carte_retournee = NULL;
+    while ((carte_retournee = retourner_carte(p)) != NULL)//Condition sur la valeur de l'affectation: si retourner_carte renvoie NULL, cela signifie qu'il n'y a plus de cartes à retourner.
+    {
+        affiche_effets(carte_retournee);
+        affiche_plateau(p);
+    }
 }
 
 /**
  * @brief Fonction principale qui controle le programme.
- * 
- * @param argc Nombre d'arguments passés à l'exécutable.
- * @param argv Liste de ces arguments
  * @return int Code de retour
  */
-int main(int argc, char *argv[])
+int main()
 {
     Plateau jeu = init_plateau();       //Initialise le plateau, les factions...
     while (nouvelle_manche(jeu))
     {
-        
+        phase1(jeu);
+        phase2(jeu);
     }
+    affiche_vainqueur(jeu);
     libere_plateau(jeu);
     jeu = NULL;
     return 0;
