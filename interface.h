6617836772f15e5ae47b97cff7b896c151d52623
Thomas Roiseux <thomas.roiseux@ensiie.fr>
2022-03-18 15:14:40 +0100

    [Tâche A.4] Ajout de la dernière fonciton

--- interface.h
+++ interface.h
@@ -17,7 +17,6 @@
  * @brief Fonction qui affiche le plateau de jeu
  * @param p plateau de jeu (pointeur devant être valide)
  * @return rien
- * @author Thomas Roiseux
  */
 void affiche_plateau(Plateau p);
 
@@ -25,13 +24,11 @@ void affiche_plateau(Plateau p);
  * @brief Fonction qui affiche la main de la faction pointée par f
  * @return rien
  * @param f faction en jeu
- * @author Thomas Roiseux
  */
 void affiche_main(Faction f);
 
 /**
  * @brief Fonction qui demande si la faction pointée par f veut utiliser son option
- * @author Thomas Roiseux
  * @param f faction en jeu
  * @return 0 non
  * @return 1 oui
@@ -40,7 +37,6 @@ int utiliser_option(Faction f);
 
 /**
  * @brief Fonction qui demande la carte qui sera posée face cachée sur le plateau.
- * @author Thomas Roiseux
  * @param f faction en jeu
  * @return Carte carte choisie par la faction f et posée sur le plateau.
  */
@@ -48,7 +44,6 @@ Carte a_poser_face_cachee_sur_plateau(Faction f);
 
 /**
  * @brief Affiche les effets de la carte c si elle est posée et retournée sur le plateau p.
- * @author Thomas Roiseux
  * @param p Plateau de jeu.
  * @param c Carte dont on veut afficher les effets
  */
@@ -56,8 +51,14 @@ void affiche_effets(Plateau p, Carte c);
 
 /**
  * @brief Affiche le vainquer de la partie.
- * @author Thomas Roiseux
  * @param p Plateau de jeu.
  */
 void affiche_vainqueur(Plateau p);
+
+/**
+ * @brief Fonction qui demande à quelle position doit être placée la carte
+ * 
+ * @param p Plateau de jeu.
+ */
+void demande_position(Plateau p);
 #endif //INTERFACE_H
