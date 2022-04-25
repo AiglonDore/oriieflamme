7d813150cb2f08c0ad67105899c71bde443ef5a0
AiglonDore <thomasr.social@outlook.com>
2022-04-25 12:08:34 +0200

    [Tâche B.8] Ecriture de la fonction demande_position

--- src/interface.c
+++ src/interface.c
@@ -113,21 +113,296 @@ void affiche_vainqueur(Plateau p)
     {
         printf("Le vainqueur de la partie est : \"%s\".\n",get_nom_faction(fct.right));
     }
-    printf("Mais cette victolire n'est pas encore officialisée!\n");
+    printf("Mais cette victoire n'est pas encore officialisée!\n");
     printf("Il faut encore attendre le jury de validation pour que la victoire soit validée et définitive.\n");
     sleep(1000);
     printf("Mail de Laurent Prével: jury de validation de la victoire\n");
     printf("Le jury va se réunir et évaluer la partie afin de valider ou non votre victoire... Ah, il manque un représentant de la DFP. Où est Julien Forest?\n");
     sleep(3000);
-    printf("Julien Forest vient de se reveiller (son café n'a pas réussi à l'empêcher de dormir). Il va donc pouvoir présider le jury pour valider votre victoire!");
+    printf("Ca sent la cigarette! L'Homme des Cavernes, pardon, Julien Forest arrive par l'ascenceur (son café n'a pas réussi à l'empêcher de dormir). Il va donc pouvoir présider le jury pour valider votre victoire!");
 }
 
 Coord demande_position(Plateau p)
 {
-    Coord ret;
-    ret.i = 0;
-    ret.j = 0;
+    Coord previous = get_derniere_carte_posee(p);
+    if (previous.i == -1 && previous.j == -1)                                               //On n'affiche pas le plateau car il n'y a aucune carte posée.
+    {
+        Coord ret;
+        ret.i = 0;
+        ret.j = 0;
+        printf("Vous êtes le premier à jouer! Posez une carte où vous voulez!!");
+        printf("Absisse: ");
+        while(scanf("%d",&(ret.i)) == 0 || ret.i < 0 || ret.i > 128)
+        {
+            printf("Veuillez saisir un nombre entier compris entre 0 et 128: ");
+        }
+        printf("Ordonnée: ");
+        while(scanf("%d",&(ret.j)) == 0 || ret.j < 0 || ret.j > 128)
+        {
+            printf("Veuillez saisir un nombre entier compris entre 0 et 128: ");
+        }
+        return ret;
+    }
+    printf("Veuillez choisir où vous souhaitez poser une carte en saisissant le numéro associé:\n");
     affiche_plateau(p);
-
-    return ret;
+    int counter = 0;
+    Coord available[129*129];
+    for (int i = 0; i < 129; i++)
+    {
+        for (int j = 0; j < 129; j++)
+        {
+            if (get_plateau_jeu(p)[i][j] == NULL)//Si la case n'est pas nulle, alors on ne peut pas y poser de carte
+            {
+                if (i == 0 && j == 0)//Coin en haut à gauche
+                {
+                    if (get_plateau_jeu(p)[i][j+1] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j+1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j+1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i+1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i+1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i+1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+                else if (i == 0 && j == 128)//Coin en haut à droite
+                {
+                    if (get_plateau_jeu(p)[i+1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i+1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i+1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i-1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i-1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i-1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+                else if (i == 128 && j == 0)//Coin en bas à gauche
+                {
+                    if (get_plateau_jeu(p)[i-1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i-1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i-1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i][j+1] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j+1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j+1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+                else if (i == 128 && j == 128)//Coin en bas à droite
+                {
+                    if (get_plateau_jeu(p)[i][j-1] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j-1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j-1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i-1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i-1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i-1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+                else if (i == 0)//Première ligne hors coins
+                {
+                                        if (get_plateau_jeu(p)[i][j+1] == NULL)//Point intérieur
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j+1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j+1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i+1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i+1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i+1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i][j-1] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j-1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j-1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+                else if (i == 128)//Dernière ligne hors coins
+                {
+                    if (get_plateau_jeu(p)[i][j+1] == NULL)//Point intérieur
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j+1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j+1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i][j-1] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j-1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j-1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i-1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i-1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i-1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+                else if (j == 0)//Première colonne hors coins
+                {
+                    if (get_plateau_jeu(p)[i][j+1] == NULL)//Point intérieur
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j+1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j+1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i+1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i+1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i+1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i-1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i-1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i-1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+                else if (j == 128)//Dernière colonne hors coins
+                {
+                    if (get_plateau_jeu(p)[i+1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i+1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i+1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i][j-1] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j-1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j-1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i-1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i-1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i-1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+                else//Points intérieurs
+                {
+                    if (get_plateau_jeu(p)[i][j+1] == NULL)//Point intérieur
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j+1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j+1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i+1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i+1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i+1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i][j-1] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i;
+                        tmp.j=j-1;
+                        printf("%d: (%d,%d)\n",counter+1,i,j-1);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                    if (get_plateau_jeu(p)[i-1][j] == NULL)
+                    {
+                        Coord tmp;
+                        tmp.i=i-1;
+                        tmp.j=j;
+                        printf("%d: (%d,%d)\n",counter+1,i-1,j);
+                        available[counter] = tmp;
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+    int item = 0;
+    while (scanf("%d",&item) == 0 || item < 1 || item > counter)
+    {
+        printf("Veuillez saisir un nombre entier compris entre 1 et %d: ",counter);
+    }
+    return available[item - 1];
 }
